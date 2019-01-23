de36456752dd2dcd6eed972c750020166ea18c8c
John Alvaro <jhon54plex@hotmail.com>
2019-01-22 22:10:02 -0500

    Inicio de pagina laboftal

---
+++ resources/views/index.blade.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Laboratorios Oftálmicos</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+</head>
+
+<!-- ______________________________________________________________________-->
+
+<body id="page-top">
+    <div id="home"></div>
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header" >
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">nav</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top" ><img class="logo_empresa" src="img/logo.png"></a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#home">HOME</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#productos">PRODUCTOS</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="servicio_tecnico.html">SERVICIO TÉCNICO</a>
+                    </li>
+                    <li>
+                       <a class="page-scroll" href="#contact">CONTACTO</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+<!-- ______________________________________________________________________-->
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <hr class="hr_header">
+                <p>Equipos de alta calidad con</p>
+                <h1 id="homeHeading"><i>TECNOLOGÍA</i></h1>
+                <h1 class="seg_tlt">Avanzada</h1>
+            </div>
+        </div>
+    </header>
+
+<!-- ______________________________________________________________________-->
+
+    <section class="panel_uno" id="">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading"><i>NOSOTROS</i></h2>
+                    <hr class="light">
+                    <p class="text-faded prrf_panel_uno">LABORATORIOS OFTÁLMICOS S.A.S. es una organización dedicada a ofrecer a la comunidad oftalmológica desde hace más de 20 años de experiencia en la industria, productos de diagnóstico y alta tecnología, siendo su mayor objetivo brindar una atención personalizada con una marcada orientación a la excelencia en el servicio y la capacitación.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+<!-- ______________________________________________________________________-->
+
+    <section class="panel_dos" id="productos">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 text-center pull-right txt_panel_dos">
+                    <h2 class="h2_uno"><i>NUESTROS<i></h2>
+                    <h2 class="h2_dos">Productos</h2>
+                    <div class="btn_text_panel_dos"><a href="productos.html" style="color: white; text-decoration: none;">ENTRAR</a></div>
+                    <div class="btn_fondo_panel_dos"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<!-- _____________________________________________________________________-->
+    <section id="" class="panel_tres">
+        <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2 text-center">
+                        <h2 class="section-heading"><i>CONTÁCTANOS</i></h2>
+                        <hr class="light">
+                    </div>
+                </div> <!-- cierre div.(row) -->
+        </div> <!-- cierre div.(contenedor_grafica) -->
+    </section>
+<!-- ____________________________________________________________________-->
+
+   <section class="panel_cuatro" id="contact">
+       <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 text-center">
+                    <form role="form" action="email.php" method="POST">
+                        <input type="text" class="formulario-control" id="name" name="nombre" placeholder="NOMBRE" required="true" />
+
+                        <input type="email" class="formulario-control" id="email" name="correo" placeholder="TEL FIJO" required="true"/>
+
+                        <input type="email" class="formulario-control" id="email" name="correo" placeholder="CELULAR" required="true"/>
+
+                        <input type="email" class="formulario-control" id="email" name="correo" placeholder="EMAIL" required="true"/>
+
+                        <textarea rows="5" cols="50" class="formulario-control" id="message" name="mensaje" placeholder="ASUNTO" required="true"></textarea>
+
+                        <button type="submit" class="btn btn-primary pull-right">ENVIAR</button>
+                    </form>
+                </div><!-- cierre div.(Col-lg-4)-->
+            </div><!-- cierre div.row -->
+       </div> <!-- cierre div.cintainer -->
+    </section>
+<!-- _________________________________________________________________-->
+
+    <section class="bloque_separador">
+
+    </section>
+<!-- _________________________________________________________________-->
+
+   <footer class="footer" id="contact">
+       <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 text-center box_izq">
+                    <img src="img/logo.png" alt="Logo Laboratorios Oftalmológicos" class="logo_laboratorio">
+                    <div class="row row_logos">
+                        <div class="logos_redes logo_facebook">
+                            <i class="fa fa-facebook fa-2x sr-contact"></i>
+                        </div>
+                        <div class="logos_redes logo_instagram">
+                            <i class="fa fa-instagram fa-2x sr-contact"></i>
+                        </div>
+                    </div>
+                </div><!-- cierre div.(Col-lg-4)-->
+
+
+                <div class="col-lg-4  col-md-6 text-center box_md" >
+                    <h2 class="">CONTACTO</h2>
+                    <div class="box_md_bloque">
+                        <div class="col-lg-2 col-xs-2 box_md_izq">
+                            <i class="fa fa-map-marker fa-2x sr-contact"></i>
+                        </div>
+                        <div class="col-lg-10 col-xs-10 box_md_der">Calle 98 # 14-17 Edificio Parque 98. Oficina 203
+                        </div>
+                    </div>
+                    <div class="box_md_bloque">
+                        <div class="col-lg-2 col-xs-2 box_md_izq">
+                            <i class="fa fa-whatsapp fa-2x sr-contact"></i></div>
+                        <div class="col-lg-10 col-xs-10 box_md_der" style="padding-top: 7px;">317 624 3475</div>
+                    </div>
+                    <div class="box_md_bloque">
+                        <div class="col-lg-2 col-xs-2 box_md_izq">
+                            <i class="fa fa-envelope fa-2x sr-contact"></i></div>
+                        <div class="col-lg-10 col-xs-10 box_md_der" style="padding-top: 7px">gerencia@laboftal.com</div>
+                    </div>
+                    <div class="box_md_bloque pull-left" style="font-size: 17px; color: #009BA4;">
+                        <i>Encuentranos en Google Maps<i>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-12 box_der">
+                    <img src="img/logo_group.png" alt="">
+                </div>
+
+            </div><!-- cierre div.row -->
+       </div> <!-- cierre div.cintainer -->
+    </footer>
+
+
+<!-- _________________________________________________________________-->
+
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
