5cdcd0f5ec139115d818e2c7cce0e984f6e0bb92
John Alvaro <jhon54plex@hotmail.com>
2019-04-23 17:46:18 -0500

    Reparando iconos footer

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -63,13 +63,14 @@
 
 <!-- _________________________________________________________________-->
 </div>
+<script src="/js/app.js"></script>
     <script src="/vendor/jquery/jquery.min.js"></script>
     <script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
     <script src="/vendor/scrollreveal/scrollreveal.min.js"></script>
     <script src="/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
     <script src="/js/creative.min.js"></script>
-    <script src="/js/app.js"></script>
+    
 
 </body>
 
