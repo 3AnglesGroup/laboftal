ecf0ec36ce9fea8ca4675d67f2897bfd0edfc30e
John Alvaro <jhon54plex@hotmail.com>
2019-06-18 15:46:38 -0500

    Agregando cambios

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -20,18 +20,18 @@
                      <div class="col-lg-2 col-xs-2 box_md_izq">
                          <i class="fa fa-map-marker fa-2x sr-contact"></i>
                      </div>
-                     <div class="col-lg-10 col-xs-10 box_md_der">Calle 98 # 14-17 Edificio Parque 98. Oficina 203
+                     <div class="col-lg-10 col-xs-10 box_md_der">Calle 98 # 14-17 Edificio Parque 98. Oficina 203 <br> Bogota - Colombia
                      </div>
                  </div>
                  <div class="box_md_bloque">
                      <div class="col-lg-2 col-xs-2 box_md_izq">
                          <i class="fa fa-whatsapp fa-2x sr-contact"></i></div>
-                     <div class="col-lg-10 col-xs-10 box_md_der" style="padding-top: 7px;">317 624 3475</div>
+                     <div class="col-lg-10 col-xs-10 box_md_der" style="padding-top: 7px;">3176423475 - 7500343</div>
                  </div>
                  <div class="box_md_bloque">
                      <div class="col-lg-2 col-xs-2 box_md_izq">
                          <i class="fa fa-envelope fa-2x sr-contact"></i></div>
-                     <div class="col-lg-10 col-xs-10 box_md_der" style="padding-top: 7px">gerencia@laboftal.com</div>
+                     <div class="col-lg-10 col-xs-10 box_md_der" style="padding-top: 7px">ventas@laboftal.com.co</div>
                  </div>
                  <div class="box_md_bloque pull-left box_md_txt" style="font-size: 17px; color: #009BA4;">
                      Encuentranos en Google Maps
@@ -42,7 +42,8 @@
                 <div class="caja_logo_3angles">
                     <p class="txt_boxder_uno">Created By</p>
                     <img class="logo3" src="/img/logo_group.png" alt="">
-                    <p class="txt_boxder_dos">www.3AnglesGroup.com</p>
+                    <a href="http://3anglesgroup.com/"><p style="color:white" class="txt_boxder_dos">www.3AnglesGroup.com</p></a>
+                    
                 </div>
              </div>
 
